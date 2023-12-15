648c51c8d0b7d54aadf25688c1394c0530761bcf
technoyap <83795023+r4ty34r@users.noreply.github.com>
2023-12-14 21:46:38 -0500

    Add files via upload
    Successful build. NavivagtionLink Destination unable to load. API key omitted in MapView.swift

---
+++ MAPTESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAPTESTApp.swift
+//  MAPTEST
+//
+//  Created by Oscar Bruno on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct MAPTESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
